69885b891b5516fae923f0c1a881b86637e19183
Arista Wahyu Saputra <aristawahyusaputra@hotmail.com>
2025-12-21 01:51:21 +0700

    Commit 251221 - Publish Go Live

--- BBS_DI/Models/_EF/Tx_TransferRequest.cs
+++ BBS_DI/Models/_EF/Tx_TransferRequest.cs
@@ -42,5 +42,6 @@ namespace BBS_DI.Models._EF
         public Nullable<int> CreatedUser { get; set; }
         public Nullable<System.DateTime> ModifiedDate { get; set; }
         public Nullable<int> ModifiedUser { get; set; }
+        public string ApprovalMessages { get; set; }
     }
 }
